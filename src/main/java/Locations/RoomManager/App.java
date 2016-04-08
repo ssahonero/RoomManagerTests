e9447168231284a9cfab29b60fab8e3d54348d61
SamuelSahonero <ssahonero@gmail.com>
2016-04-08 19:34:08 -0400

    First Commit: Clean Project

---
+++ src/main/java/Locations/RoomManager/App.java
@@ -0,0 +1,13 @@
+package Locations.RoomManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
